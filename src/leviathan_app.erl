d6ce7fc878f8ea86f65f657d17387e2ecaed32df
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2015-11-30 17:26:29 +0100

    Do not run Dobby on slave nodes
    Dobby is specified as an included application in the application
    resource file of Leviathan. It is started only on the master node
    and the slave nodes communicate with Dobby via globally registered
    process.

---
+++ src/leviathan_app.erl
@@ -0,0 +1,32 @@
+-module(leviathan_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    setup_dobby(),
+    leviathan_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% ===================================================================
+%% Internal functions
+%% ===================================================================
+
+setup_dobby() ->
+    case application:get_env(leviathan, master_node, undefined) of
+        undefined ->
+            %% we are the master => start dobby
+            {ok ,_ } = application:ensure_all_started(dobby);
+        MasterNode ->
+            true = net_kernel:connect(MasterNode),
+            ok = global:sync()
+    end.
+
