90a14dbe306e160c381ba0e557b03a47153b58b7
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2015-12-07 15:49:58 +0100

    Add module for Executive and improve sys.config

---
+++ src/lev_executive.erl
@@ -0,0 +1,58 @@
+-module(lev_executive).
+-behaviour(gen_server).
+-define(SERVER, ?MODULE).
+
+%% ------------------------------------------------------------------
+%% API Function Exports
+%% ------------------------------------------------------------------
+
+-export([start_link/0]).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Exports
+%% ------------------------------------------------------------------
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Definitions
+%% ------------------------------------------------------------------
+
+init(Args) ->
+    ok = net_kernel:monitor_nodes(true),
+    {ok, Args}.
+
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info({nodeup, Node}, State) ->
+    lager:info("Node ~p connected~n", [Node]),
+    {noreply, State};
+handle_info({nodedown, Node}, State) ->
+    lager:info("Node ~p disconnected~n", [Node]),
+    {noreply, State};
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
+
--- src/leviathan_sup.erl
+++ src/leviathan_sup.erl
@@ -23,5 +23,8 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+    Children = [
+                ?CHILD(lev_executive, worker)
+               ],
+    {ok, { {one_for_one, 5, 10}, Children} }.
 
